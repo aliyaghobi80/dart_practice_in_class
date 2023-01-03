8dac0c26567f3a4061f23a69efc98481824800bb
aliyaghobi80 <aliymix07@gmail.com>
2023-01-03 23:03:45 +0330

    add a new pattern

--- star_pattern.dart
+++ star_pattern.dart
@@ -98,4 +98,31 @@ void main(){
   }
 
 
+  for(int i=0;i<number;i++){
+    var star='';
+    //space
+    for(int j=number-i;j>0;j--){
+      star +="  ";
+    }
+    // print stars
+    for(int s=0;s<=i;s++){
+      star+="* ";
+    }
+    print(star);
+
+  }
+  for(int i=number;i>=0;i--){
+    var star='';
+    //space
+    for(int j=number-i;j>0;j--){
+      star +="  ";
+    }
+    // print stars
+    for(int s=0;s<=i;s++){
+      star+="* ";
+    }
+    print(star);
+
+  }
+
 }
