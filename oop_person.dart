b33024b60a26a3b2137e3db86c6db57386c82804
aliyaghobi80 <aliymix07@gmail.com>
2023-01-08 16:11:15 +0330

    get and set

---
+++ oop_person.dart
@@ -0,0 +1,26 @@
+
+class Person {
+ Person({required this.birthYear});
+  String? name;
+  String? family;
+  int birthYear;
+  // int? _birthDate;
+
+
+  int get age {
+    return 1401-birthYear;
+  }
+
+  // void set birthDate(int inputBirthDate ) {
+  //   _birthDate = inputBirthDate;
+  // }
+
+
+}
+
+main() {
+Person ali= Person(birthYear: 1380);
+ali.name="ali";
+print("name: ${ali.name} age is: ${ali.age}");
+
+}
